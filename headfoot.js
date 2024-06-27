0f00419f6d510d67ea98727a2229d57a3a3b5e7e
Sylvia <117012224+sz5ylv1a@users.noreply.github.com>
2024-06-27 16:27:42 +0200

    v0.0.15a update!
    welp, now that my finals are done, time to get back to the fun!!
    and that's exactly what i did!!

---
+++ headfoot.js
@@ -0,0 +1,19 @@
+document.querySelector("header").innerHTML = `
+<div class="menu">
+	<div class="logo"><a href="/"><img src="resources/images/placeholder-logo.png" alt="sz5ylv1a" width=214 height=54></a></div>
+	<div class="dd">
+	    <a href="javascript:void(0)" class="ddbtn">≡</a>
+	    <div class="menu-items">
+		    <a href="/">Home</a>
+		    <a href="about.html">About Me</a>
+		    <a href="socials.html">Socials</a>
+		    <a href="changelog.html">Changelog</a>
+	    </div>
+	</div>
+</div>
+`;
+
+document.querySelector("footer").innerHTML = `
+<div class="currentver">v0.0.15a • Last updated: 06/27/2024 • <a href="https://github.com/sz5ylv1a/sz5ylv1a.github.io">Source Code</a></div>
+<div class="fakecrdate">sz5ylv1a 2017-2024</div>
+`;
